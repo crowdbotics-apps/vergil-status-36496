1d81cc6b545667468dcab67ef50d3e859ea33e0b
Crowdbotics <team@crowdbotics.com>
2022-07-30 01:12:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vergil_status_36496",
+  url: "https://vergil_status_36496.botics.co",
+  api: "https://vergil_status_36496.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
